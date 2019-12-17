4b9950a11b43146bff9dc5f95fdc10a95113cc95
phatdroid94 <phatdroid94@gmail.com>
2019-12-17 21:34:45 +0700

    composite

--- app/src/main/java/com/example/roomdatabase08102019/MainViewModel.java
+++ app/src/main/java/com/example/roomdatabase08102019/MainViewModel.java
@@ -3,19 +3,24 @@ package com.example.roomdatabase08102019;
 import android.annotation.SuppressLint;
 import android.content.Context;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainViewModel extends ViewModel implements LifecycleObserver {
 
+    private CompositeDisposable disposable = new CompositeDisposable();
     private MutableLiveData<List<Sinhvien>> listSinhvien = new MutableLiveData<>();
     @SuppressLint("CheckResult")
     public LiveData<List<Sinhvien>> getListSinhvien(Context context){
@@ -24,13 +29,33 @@ public class MainViewModel extends ViewModel implements LifecycleObserver {
                 .getAllSinhVien()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<Sinhvien>>() {
+                .subscribe(new Observer<List<Sinhvien>>() {
                     @Override
-                    public void accept(List<Sinhvien> sinhviens) throws Exception {
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(List<Sinhvien> sinhviens) {
                         listSinhvien.setValue(sinhviens);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
                     }
                 });
         return listSinhvien;
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void disableListenData(){
+        disposable.clear();
     }
 
 
